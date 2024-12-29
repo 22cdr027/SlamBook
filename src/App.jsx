e5a1b912e21e480c9e19518c3a6c748bc23b602c
HARIHARAN S <151810903+22cdr027@users.noreply.github.com>
2024-12-29 20:56:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+  return (
+    <>
+    <h1>
+      Slam Book
+    </h1>
+    </>
+  )
+}
+
+export default App
+
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+import Form from './Form.jSX'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <Form/>
+  </StrictMode>,
+)
+
